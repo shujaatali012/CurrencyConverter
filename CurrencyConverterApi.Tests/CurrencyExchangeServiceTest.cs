b96cde494f12c7169ab969a9c5177e0867fe6302
shujaatali012 <shujaat.ali012@gmail.com>
2025-01-30 18:22:18 +0400

    make the test working and test currency coversion service

--- CurrencyConverterApi.Tests/CurrencyExchangeServiceTest.cs
+++ CurrencyConverterApi.Tests/CurrencyExchangeServiceTest.cs
@@ -1,7 +1,12 @@
 ﻿using Xunit;
 using Microsoft.Extensions.Configuration;
-using CurrencyConverterApi.Services;
+using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System;
+
+using CurrencyConverterApi.Services;
 
 namespace CurrencyConverterApi.Tests;
 
@@ -9,13 +14,36 @@ public class CalculatorServiceTests
 {
     private readonly CurrencyExchangeService _currencyExchangeService;
     private readonly IConfiguration _configuration;
+    private readonly IHttpClientFactory _httpClientFactory;
     
-
-    public CalculatorServiceTests(IConfiguration configuration)
+    public CalculatorServiceTests()
     {
-        HttpClient httpClient = new HttpClient();
-        IFrankfurterExchangeService primaryCurrecyExchangeService = new FrankfurterExchangeService(httpClient, configuration);
-        IFixerExchangeService backupCurrecyExchangeService = new FixerExchangeService(httpClient, configuration);
+        // setup dependency injection container
+        var serviceCollection = new ServiceCollection();
+
+        // in-memory configuration
+        var configValues = new Dictionary<string, string>
+            {
+                { "ExchangeApiKeys:FrankfurterLatestEndpoint", "https://api.frankfurter.app/latest" },
+                { "ExchangeApiKeys:FrankfurterDevEndpoint", "https://api.frankfurter.dev/v1/" },
+                { "ExchangeApiKeys:FixerLatestEndpoint", "https://data.fixer.io/api/latest" },
+                { "ExchangeApiKeys:FixerTimeSeriestEndpoint", "https://data.fixer.io/api/timeseries" },
+                { "ExchangeApiKeys:FixerKey", "" }
+            };
+
+        _configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(configValues)
+            .Build();
+
+        serviceCollection.AddSingleton<IConfiguration>(_configuration); // configuration
+        serviceCollection.AddHttpClient(); // client factory
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        _configuration = serviceProvider.GetRequiredService<IConfiguration>();
+        _httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+
+        IFrankfurterExchangeService primaryCurrecyExchangeService = new FrankfurterExchangeService(_httpClientFactory, _configuration);
+        IFixerExchangeService backupCurrecyExchangeService = new FixerExchangeService(_httpClientFactory, _configuration);
         _currencyExchangeService = new CurrencyExchangeService(primaryCurrecyExchangeService, backupCurrecyExchangeService);
     }
 
@@ -29,6 +57,6 @@ public class CalculatorServiceTests
         decimal result = 
             _currencyExchangeService.ConvertCurrencyAsync(fromCurrency, toCurrency, amount).GetAwaiter().GetResult();
         
-        Assert.Equal(104.0421m, result);
+        Assert.Equal(103.9600m, result); // check the rate and update the value
     }
 }
